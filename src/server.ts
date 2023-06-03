8e877dc9c1638210c485ad739857774b6179eec8
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-06-03 18:46:04 -0300

    feat: only tesdt

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import fastify from 'fastify'
+import fastify from "fastify"
 
-const app = fastify()
+const app = fastify();
 
 app
-  .listen({ port: 3333 })
-  .then(() => console.info('HTTP server running on http://localhost:3333'))
-  .catch((err) => console.error('Fail to run server:', err))
+.listen(
+  { port: 3333 })
+  .then(() => console.info(
+    'HTTP server running on http://localhost:3333')).catch((err) => console.error('Fail to run server:', err))
