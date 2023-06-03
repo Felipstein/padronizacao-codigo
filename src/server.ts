833ed5bd731f0bb35816a04e0ce373172a389604
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-06-03 18:41:12 -0300

    feat: initial commit

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 
 const app = fastify();
 
+console.info("ISSO AÍ ${2 + 2}");
+
 app.listen({ port: 3333 })
   .then(() => console.info('HTTP server running on http://localhost:3333'))
   .catch(err => console.error('Fail to run server:', err));
